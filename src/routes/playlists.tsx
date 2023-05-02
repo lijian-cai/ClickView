d759a64709c76da81bcf95413eb99d4e88e7eee1
Tony Cai <cailijian1991@gmail.com>
2023-05-02 14:14:02 +0930

    Task 2 display videos and playlists

--- src/routes/playlists.tsx
+++ src/routes/playlists.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { PlaylistItem } from "../components/playlist-item";
+
+import { Playlist } from '../interfaces/playlist';
+
 export function Playlists() {
+  const [playlists, setPlaylists] = useState<Array<Playlist>>([]);
+
+  useEffect(() => {
+    const fetchPlaylists = async () => {
+      const result = await fetch('./playlists.json');
+      const _playlists = await result.json()
+      setPlaylists(_playlists);
+    }
+
+    fetchPlaylists()
+  }, [])
+
   return (
     <main>
-      <h1>Playlists route</h1>
+      {playlists.map((playlist) => <PlaylistItem key={playlist.id} playlist={playlist} />)}
     </main>
   );
 }
--- src/routes/videos.tsx
+++ src/routes/videos.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import VideoItem from "../components/video-item";
+import { Video } from "../interfaces/video";
+
 export function Videos() {
+  const [videos, setVideos] = useState<Array<Video>>([]);
+
+  useEffect(() => {
+    const fetchPlaylists = async () => {
+      const result = await fetch('./videos.json');
+      const _videos = await result.json()
+      setVideos(_videos);
+    }
+
+    fetchPlaylists()
+  }, [])
+  
   return (
     <main>
-      <h1>Videos route</h1>
+      {videos.map((video) => <VideoItem key={video.id} video={video} />)}
     </main>
   )
 }
